2114dcff1727d7d34d4ce9eff9621fbbffd548e6
residante <mr.residante@gmail.com>
2012-04-11 11:36:50 +0200

    Flash with arduino_wraper

--- flash.sh
+++ flash.sh
@@ -6,7 +6,7 @@
 #           Elizabeth Paz
 # LGPL
 #
-# Syntaxe: # ./flash.sh myprogram.lua"
+# Syntaxe: # ./flash.sh [myprogram.lua]"
 #
 #################################
 # Env
@@ -14,29 +14,36 @@
 STFLASH="$(pwd)/env/utils/st-flash"
 COMMAND="sudo $STFLASH write elua_lua_stm32f407vg.bin 0x08000000"
 
-source activate_sourcery.sh
+flash() {
+    source activate_sourcery.sh
+    if test ! -d "elua"; then
+        echo Erreur: Vous devez installer elua
+        exit 1
+    fi
+    if test ! -f "arduino_wraper/arduino_wraper.lua"; then
+        echo Erreur: il manque le fichier arduino_wraper/arduino_wraper.lua
+        exit 1
+    fi
+    cp ./arduino_wraper/arduino_wraper.lua ./elua/romfs/arduino_wraper.lua
+    cd elua
+    scons board=STM32F4DSCY prog
+    echo $COMMAND
+    $COMMAND
+    cd ../
+}
 
-if [ $# -ne 1 ]
-then
-    echo Usage: $0 myprogram.lua  
-    exit 1
+## Début
+if [ $# -ne 1 ]; then
+    flash
+else
+    file=$1
+    if test ! -f $file; then
+        echo Erreur: $file "n'est pas un fichier"
+        exit 1
+    fi
+    cp $file ./elua/romfs/autorun.lua
+    flash
 fi
 
-file=$1
+echo 'we are done....'
 
-if test ! -f $file; then
-    echo Erreur: $file "n'est pas un fichier"
-    exit 1
-fi
-
-if test ! -d "elua"; then
-    echo Erreur: Vous devez installer elua
-    exit 1
-fi
-
-#rm /elua/romfs/autorun.lua
-cp $file ./elua/romfs/autorun.lua
-cd elua
-scons board=STM32F4DSCY prog
-echo $COMMAND
-$COMMAND
