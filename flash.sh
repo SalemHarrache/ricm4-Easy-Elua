5d443aac829c98e942177aabba619b7940989136
residante <mr.residante@gmail.com>
2012-04-11 10:00:28 +0200

    finish flash script

--- flash.sh
+++ flash.sh
@@ -6,21 +6,37 @@
 #           Elizabeth Paz
 # LGPL
 #
-# Syntaxe: # su - -c "./flash.sh myprogram.lua"
-# Syntaxe: or # sudo ./flash.sh myprogram.lua
+# Syntaxe: # ./flash.sh myprogram.lua"
 #
-
 #################################
 # Env
 
 STFLASH="$(pwd)/env/utils/st-flash"
-
 COMMAND="sudo $STFLASH write elua_lua_stm32f407vg.bin 0x08000000"
 
 source activate_sourcery.sh
+
+if [ $# -ne 1 ]
+then
+    echo Usage: $0 myprogram.lua  
+    exit 1
+fi
+
+file=$1
+
+if test ! -f $file; then
+    echo Erreur: $file "n'est pas un fichier"
+    exit 1
+fi
+
+if test ! -d "elua"; then
+    echo Erreur: Vous devez installer elua
+    exit 1
+fi
+
+#rm /elua/romfs/autorun.lua
+cp $file ./elua/romfs/autorun.lua
 cd elua
-rm ./romfs/autorun.lua
-wget https://raw.github.com/SalemHarrache/elua-examples/master/led/led.lua -O  ./romfs/autorun.lua
 scons board=STM32F4DSCY prog
 echo $COMMAND
 $COMMAND
