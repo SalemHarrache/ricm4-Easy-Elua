a0950d44ee83c5359e5013ec19711f10d047b03d
residante <mr.residante@gmail.com>
2012-04-11 10:49:11 +0200

    implement class in elua

--- arduino_wraper/app.lua
+++ arduino_wraper/app.lua
@@ -1,51 +1,94 @@
 
-App = {}
-App.__index = App
+--function class()
+--    local cls = {}
+--    cls.__index = cls
+--    return setmetatable(cls, {__call = function (c, ...)
+--        instance = setmetatable({}, cls)
+--        if cls.__init then
+--            cls.__init(instance, ...)
+--        end
+--        return instance
+--    end})
+--end
 
-function App:init(name)
-   self.name = name
-   return self
+Class = {}
+
+function Class:new(super)
+    local class, metatable, properties = {}, {}, {}
+    class.metatable = metatable
+    class.properties = properties
+
+    function metatable:__index(key)
+        local prop = properties[key]
+        if prop then
+            return prop.get(self)
+        elseif class[key] ~= nil then
+            return class[key]
+        elseif super then
+            return super.metatable.__index(self, key)
+        else
+            return nil
+        end
+    end
+
+    function metatable:__newindex(key, value)
+        local prop = properties[key]
+        if prop then
+            return prop.set(self, value)
+        elseif super then
+            return super.metatable.__newindex(self, key, value)
+        else
+            rawset(self, key, value)
+        end
+    end
+
+    function class:new(...)
+        local obj = setmetatable({}, self.metatable)
+        if obj.__new then
+            obj:__new(...)
+        end
+        return obj
+    end
+
+    return class
 end
 
-function App.setup()
-    return
+App = Class:new()
+
+function App:__new(name)
+    -- for debuging
+    self.name = name
 end
 
-function App.setup()
-    print (self.name)
+function App:setup()
+    print("setup")
+end
+
+function App:loop()
+    print("loop")
 end
 
 function App:run()
-    self.setup()
+    print("run")
+    self:setup()
+    while true do
+        self:loop()
+    end
 end
 
--- create and use an App
-application = App:init("New app")
 
-application:run()
+app = App:new("Main")
 
 
-
---A = class()
---function A:init(x)
---  self.t = 0
---  self.x = x
+--function App:loop()
+--    print("loop2")
 --end
 
 
---function GameNetwork:init(desiredMode)
---	self.mode = desiredMode
---	if desiredMode == "papaya" then
---		gameNetwork.init("papaya", "SecretCodez")
---	elseif desiredMode == "openfeint" then
---		gameNetwork.init("openfeint",
---					"SecretCodez",
---					"SecretCodez",
---					"Game Title",
---					"SecretCodez")
---	else
---		print("Unknown mode.")
---		return false
---	end
+--function App:setup()
+--    print("setup2")
 --end
+
+
+app:run()
 
