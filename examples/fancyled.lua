d415cbfa4179a11380587ecdcc358855ad704296
residante <mr.residante@gmail.com>
2012-04-18 18:44:59 +0200

    Add Fancy blink led example

---
+++ examples/fancyled.lua
@@ -0,0 +1,30 @@
+-- FancyLED
+require("arduino_wraper")
+
+blink_toggle = function(value)
+    digitalWrite(GREEN_LED, value)
+    delay(1000)
+    digitalWrite(ORANGE_LED, value)
+    delay(1000)
+    digitalWrite(RED_LED, value)
+    delay(1000)
+    digitalWrite(BLUE_LED, value)
+    delay(1000)
+end
+
+function App:setup()
+    pinMode(RED_LED, OUTPUT) -- Initialize the digital pin as an output.
+    pinMode(ORANGE_LED, OUTPUT)
+    pinMode(BLUE_LED, OUTPUT)
+    pinMode(GREEN_LED, OUTPUT)
+
+end
+
+function App:loop()
+    blink_toggle(HIGH)
+    blink_toggle(LOW)
+end
+
+app = App:new("Fancy Blink Led")
+app:run()
+
